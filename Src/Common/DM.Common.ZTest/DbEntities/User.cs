2b53a56cc959df888773e177986d6440a641b2ee
icmyfuture <icmyfuture@gmail.com>
2014-03-16 15:44:48 +0800

    test sqlite mapping with system.data.sqlite.dll

--- Src/Common/DM.Common.ZTest/DbEntities/User.cs
+++ Src/Common/DM.Common.ZTest/DbEntities/User.cs
@@ -4,7 +4,7 @@ namespace DM.Common.ZTest.DbEntities
 {
     public class User
     {
-        public string ID { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
         public DateTime CreatedTime { get; set; }
         public bool IsAdmin { get; set; }
